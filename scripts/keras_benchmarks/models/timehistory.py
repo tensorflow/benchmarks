b9c3f9b562423c11bd8fdcac2c9cdf4b1116e096
Anjali Sridhar <anjalisridhar@google.com>
2017-10-19 18:04:56 -0700

    add timehistory callback

---
+++ scripts/keras_benchmarks/models/timehistory.py
@@ -0,0 +1,13 @@
+import keras
+from keras.callbacks import Callback
+import time
+
+class TimeHistory(keras.callbacks.Callback):
+  def on_train_begin(self, logs={}):
+    self.times = []
+
+  def on_epoch_begin(self, batch, logs={}):
+    self.epoch_time_start = time.time()
+
+  def on_epoch_end(self, batch, logs={}):
+    self.times.append(time.time() - self.epoch_time_start)
