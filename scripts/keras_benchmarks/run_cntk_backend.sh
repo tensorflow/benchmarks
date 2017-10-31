3f90e4e88a7e381d9241e8fa5969644c63a463d1
Anjali Sridhar <anjalisridhar@google.com>
2017-10-31 14:44:49 -0700

    fix if/else statement

--- scripts/keras_benchmarks/run_cntk_backend.sh
+++ scripts/keras_benchmarks/run_cntk_backend.sh
@@ -7,9 +7,10 @@ sed -i -e 's/"backend":[[:space:]]*"[^"]*/"backend":\ "'$KERAS_BACKEND'/g' ~/.ke
 echo -e "Running tests with the following config:\n$(cat ~/.keras/keras.json)"
 
 # CPU Mode
-if $1 == "cpu":
+if [ "$1" = "cpu" ]; then
   python benchmarks/scripts/keras_benchmarks/run_benchmark.py --keras_backend=$KERAS_BACKEND --cpu_num_cores='1' --cpu_memory='3.75' --cpu_memory_info='GB' --platform_type='GCP' --platform_machine_type='n1-standard-1'
-else
+fi
+if [ "$1" = "gpu" ]; then
   #GPU Mode, only single GPU mode works for now
   python benchmarks/scripts/keras_benchmarks/run_benchmark.py --keras_backend=$KERAS_BACKEND --gpu_count='1' --gpu_platform='NVIDIA Tesla K80' --cpu_num_cores='8' --cpu_memory='30' --cpu_memory_info='GB' --platform_type='GCP' --platform_machine_type='n1-standard-8'
-endif
+fi
--- scripts/keras_benchmarks/run_theano_backend.sh
+++ scripts/keras_benchmarks/run_theano_backend.sh
@@ -7,8 +7,8 @@ sed -i -e 's/"backend":[[:space:]]*"[^"]*/"backend":\ "'$KERAS_BACKEND'/g' ~/.ke
 echo -e "Running tests with the following config:\n$(cat ~/.keras/keras.json)"
 
 # CPU Mode
-if $1 == "cpu":
+if [ "$1" = "cpu" ]; then
   python benchmarks/scripts/keras_benchmarks/run_benchmark.py --keras_backend=$KERAS_BACKEND --cpu_num_cores='1' --cpu_memory='3.75' --cpu_memory_info='GB' --platform_type='GCP' --platform_machine_type='n1-standard-1'
 else
   echo "GPU mode for Theano backend is not supported currently by the keras benchmarks script."
-endif
+fi
