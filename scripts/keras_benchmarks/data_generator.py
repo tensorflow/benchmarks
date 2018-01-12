e6aa7b76e77727ab0ec22fdb1a4910ebc47848ad
Anjali Sridhar <anjalisridhar@google.com>
2018-01-12 15:10:32 -0800

    addressed PRcomments

--- scripts/keras_benchmarks/data_generator.py
+++ scripts/keras_benchmarks/data_generator.py
@@ -7,11 +7,11 @@ def generate_img_input_data(input_shape, num_classes):
 
   # Arguments
     input_shape: input shape in the following format
-                      (num_samples,channels, x, y)
+                      `(num_samples, channels, x, y)`
     num_classes: number of classes that we want to classify the input
 
   # Returns
-    numpy arrays: 'x_train, y_train'
+    numpy arrays: `x_train, y_train`
   """
   x_train = np.random.randint(0, 255, input_shape)
   y_train = np.random.randint(0, num_classes, (input_shape[0],))
@@ -19,17 +19,26 @@ def generate_img_input_data(input_shape, num_classes):
   return x_train, y_train
 
 
-def generate_text_input_data(input_shape):
-  """Generates training data and target labels.
+def generate_text_input_data(input_shape, p=0.05, return_as_bool=True):
+  """Generates training data and target labels .
+
+  Given an input shape the function generates one hot encoded vectors. For
+  example when we use words as our tokens, the presence/absence of the given
+  word in the vocabulary is represented by True/False.
 
   # Arguments
-    input_shape: input shape in the following format(num_samples, x, y)
+    input_shape: input shape in the following format `(num_samples, x, y)`
+    p: fraction of tokens that are present in the vocabulary
+    return_as_bool: data and labels are returned as boolean arrays
 
   # Returns
-    numpy arrays: 'x_train, y_train' where the value of the arrays are booleans.
+    numpy arrays: `x_train, y_train`
   """
-  x_train = np.random.choice([True,False], input_shape)
-  y_train = np.random.choice([True,False], (input_shape[0],input_shape[2]))
+  x_train = np.random.binomial(1, p, input_shape)
+  y_train = np.random.binomial(1, p, (input_shape[0], input_shape[2]))
+
+  if return_as_bool:
+    return x_train.astype(bool), y_train.astype(bool)
 
   return x_train, y_train
 
