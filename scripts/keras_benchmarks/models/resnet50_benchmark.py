75da81147942a9cbeb0d9fddd9fce574e9912a4e
Anjali Sridhar <anjalisridhar@google.com>
2018-01-31 15:58:38 -0800

    add resnet50 model to test logits loss performance

--- scripts/keras_benchmarks/models/resnet50_benchmark.py
+++ scripts/keras_benchmarks/models/resnet50_benchmark.py
@@ -9,14 +9,16 @@ from __future__ import print_function
 import keras
 from keras import applications
 from keras.utils import multi_gpu_model
-
 from models import timehistory
 from data_generator import generate_img_input_data
 if keras.backend.backend() == 'cntk':
   from gpu_mode import cntk_gpu_mode_config, finalize
 
 if keras.backend.backend() == 'tensorflow':
-  from tensorflow.contrib.data import Dataset
+  import tensorflow as tf
+
+def crossentropy_from_logits(y_true, y_pred):
+  return tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
 
 class Resnet50Benchmark:
 
@@ -24,10 +26,13 @@ class Resnet50Benchmark:
     self.test_name = "resnet50"
     self.sample_type = "images"
     self.total_time = 0
-    self.batch_size = 64
+    self.batch_size = 16
     self.epochs = 4
     self.num_samples = 1000
     self.test_type = 'channels_first, batchnorm, learning_phase, batch_size'
+
+    self.use_dataset_tensors = False
+    self.use_logits_loss = True
 
   def run_benchmark(self, gpus=0):
     print("Running model ", self.test_name)
@@ -41,60 +46,27 @@ class Resnet50Benchmark:
       keras.backend.set_image_data_format('channels_first')
 
     keras.backend.set_learning_phase(True)
-
-    resnet_model = applications.ResNet50(weights=None)
-
     y_train = keras.utils.to_categorical(y_train, num_classes)
 
     if keras.backend.image_data_format() == 'channels_last':
       x_train = x_train.transpose(0, 2, 3, 1)
 
-    x_train = x_train.astype('float32')
     x_train /= 255
 
-    # Create the dataset and its associated one-shot iterator.
-    dataset = Dataset.from_tensor_slices((x_train, y_train))
-    dataset = dataset.repeat()
-    dataset = dataset.shuffle(10000)
-    dataset = dataset.batch(self.batch_size)
-    iterator = dataset.make_one_shot_iterator()
+    input_tensor = keras.layers.Input(tensor=keras.backend.constant(x_train))
+    base_model = applications.ResNet50(input_tensor=input_tensor, include_top=False, pooling='avg', weights=None)
+    output = base_model.output
+    predictions = keras.layers.Dense(num_classes)(output)
+    model = keras.models.Model(input_tensor, predictions)
 
-    # Model creation using tensors from the get_next() graph node.
-    inputs, targets = iterator.get_next()
-    model_input = keras.layers.Input(tensor=inputs)
-    model_output = resnet_model(model_input)
-    model = keras.models.Model(inputs=model_input, outputs=model_output)
-
-
-    opt = keras.optimizers.rmsprop(lr=0.0001, decay=1e-6)
-
-    if keras.backend.backend() == "tensorflow" and gpus > 1:
-      model = multi_gpu_model(model, gpus=gpus)
-
-    # model.compile(loss='categorical_crossentropy',
-    #               optimizer=opt,
-    #               metrics=['accuracy'])
     model.compile(optimizer=keras.optimizers.RMSprop(lr=2e-3, decay=1e-5),
-                  loss='categorical_crossentropy',
-                  metrics=['accuracy'],
-                  target_tensors=[targets])
-
-    # create a distributed trainer for cntk
-    if keras.backend.backend() == "cntk" and gpus > 1:
-      start, end = cntk_gpu_mode_config(model, x_train.shape[0])
-      x_train = x_train[start: end]
-      y_train = y_train[start: end]
+                  loss=crossentropy_from_logits,
+                  metrics=['accuracy'])
 
     time_callback = timehistory.TimeHistory()
 
-    # model.fit(x_train,
-    #                     y_train,
-    #                     batch_size=self.batch_size,
-    #                     epochs=self.epochs,
-    #                     shuffle=True,
-    #                     callbacks=[time_callback])
-    model.fit(epochs=self.epochs,
-                    steps_per_epoch=10)
+    model.fit(y=y_train, batch_size=self.batch_size,
+              epochs=self.epochs, shuffle=True, callbacks=[time_callback])
 
     self.total_time = 0
     for i in range(1, self.epochs):
