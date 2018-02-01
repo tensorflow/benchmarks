bb876c75f440c7a5c2eef183f113453da7a21b55
Anjali Sridhar <anjalisridhar@google.com>
2018-02-01 14:41:07 -0800

    add logits loss change

--- scripts/keras_benchmarks/models/resnet50_benchmark.py
+++ scripts/keras_benchmarks/models/resnet50_benchmark.py
@@ -9,16 +9,14 @@ from __future__ import print_function
 import keras
 from keras import applications
 from keras.utils import multi_gpu_model
+
 from models import timehistory
 from data_generator import generate_img_input_data
+if keras.backend.backend() == 'tensorflow':
+  import tensorflow as tf
 if keras.backend.backend() == 'cntk':
   from gpu_mode import cntk_gpu_mode_config, finalize
 
-if keras.backend.backend() == 'tensorflow':
-  import tensorflow as tf
-
-def crossentropy_from_logits(y_true, y_pred):
-  return tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
 
 class Resnet50Benchmark:
 
@@ -26,47 +24,88 @@ class Resnet50Benchmark:
     self.test_name = "resnet50"
     self.sample_type = "images"
     self.total_time = 0
-    self.batch_size = 16
+    self.batch_size = 32
     self.epochs = 4
     self.num_samples = 1000
     self.test_type = 'channels_first, batchnorm, learning_phase, batch_size'
-
     self.use_dataset_tensors = False
     self.use_logits_loss = True
 
+  def crossentropy_from_logits(y_true, y_pred):
+    return tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
+
   def run_benchmark(self, gpus=0):
     print("Running model ", self.test_name)
+
+    keras.backend.set_learning_phase(True)
 
     input_shape = (self.num_samples, 3, 224, 224)
     num_classes = 1000
 
     x_train, y_train = generate_img_input_data(input_shape, num_classes)
+    if self.use_logits_loss:
+      x_train = x_train.astype('float32')
+      y_train = y_train.astype('float32')
+    y_train = keras.utils.to_categorical(y_train, num_classes)
 
     if keras.backend.backend == "tensorflow" and gpus > 1:
       keras.backend.set_image_data_format('channels_first')
-
-    keras.backend.set_learning_phase(True)
-    y_train = keras.utils.to_categorical(y_train, num_classes)
 
     if keras.backend.image_data_format() == 'channels_last':
       x_train = x_train.transpose(0, 2, 3, 1)
 
     x_train /= 255
 
-    input_tensor = keras.layers.Input(tensor=keras.backend.constant(x_train))
-    base_model = applications.ResNet50(input_tensor=input_tensor, include_top=False, pooling='avg', weights=None)
-    output = base_model.output
-    predictions = keras.layers.Dense(num_classes)(output)
-    model = keras.models.Model(input_tensor, predictions)
+    if self.use_dataset_tensors:
+      # Create the dataset and its associated one-shot iterator.
+      dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
+      dataset = dataset.repeat()
+      dataset = dataset.shuffle(10000)
+      dataset = dataset.batch(self.batch_size)
+      iterator = dataset.make_one_shot_iterator()
 
-    model.compile(optimizer=keras.optimizers.RMSprop(lr=2e-3, decay=1e-5),
-                  loss=crossentropy_from_logits,
-                  metrics=['accuracy'])
+      # Model creation using tensors from the get_next() graph node.
+      inputs, targets = iterator.get_next()
+
+    if self.use_dataset_tensors:
+      input_tensor = keras.layers.Input(tensor=inputs)
+      outputs = applications.ResNet50(include_top=False, pooling='avg', weights=None)(input_tensor)
+      predictions = keras.layers.Dense(num_classes)(outputs)
+      model = keras.models.Model(input_tensor, predictions)
+    else:
+      model = applications.ResNet50(weights=None)
+
+    if keras.backend.backend() == "tensorflow" and gpus > 1:
+      model = multi_gpu_model(model, gpus=gpus)
+
+    if self.use_logits_loss:
+      loss = self.crossentropy_from_logits
+    else:
+      loss = 'categorical_crossentropy'
+
+    if self.use_dataset_tensors:
+      model.compile(loss=loss,
+                    optimizer=keras.optimizers.rmsprop(lr=0.0001, decay=1e-6),
+                    metrics=['accuracy'],
+                    target_tensors=[targets])
+    else:
+      model.compile(loss=loss,
+                    optimizer=keras.optimizers.rmsprop(lr=0.0001, decay=1e-6),
+                    metrics=['accuracy'])
+
+    # create a distributed trainer for CNTK
+    if keras.backend.backend() == "cntk" and gpus > 1:
+      start, end = cntk_gpu_mode_config(model, x_train.shape[0])
+      x_train = x_train[start: end]
+      y_train = y_train[start: end]
 
     time_callback = timehistory.TimeHistory()
 
-    model.fit(y=y_train, batch_size=self.batch_size,
-              epochs=self.epochs, shuffle=True, callbacks=[time_callback])
+    if self.use_dataset_tensors:
+      model.fit(epochs=self.epochs, steps_per_epoch=15, callbacks=[time_callback])
+    else:
+      model.fit(x_train, y_train, batch_size=self.batch_size, epochs=self.epochs,
+                shuffle=True, callbacks=[time_callback])
 
     self.total_time = 0
     for i in range(1, self.epochs):
--- scripts/keras_benchmarks/run_tf_backend.sh
+++ scripts/keras_benchmarks/run_tf_backend.sh
@@ -8,8 +8,9 @@ echo -e "Running tests with the following config:\n$(cat ~/.keras/keras.json)"
 
 # Use "cpu_config", "gpu_config" and "multi_gpu_config" as command line arguments to load the right
 # config file.
-models='cifar10_cnn gru lstm mnist_mlp resnet50 vgg16 xception'
+#models='cifar10_cnn gru lstm mnist_mlp resnet50 vgg16 xception'
+models='resnet50'
 for name in $models
 do
-  python benchmarks/scripts/keras_benchmarks/run_benchmark.py  --mode="$1" --model_name="$name" --dry_run=
+  python benchmarks/scripts/keras_benchmarks/run_benchmark.py  --mode="$1" --model_name="$name" --dry_run=True
 done
