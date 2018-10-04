tensorflow/benchmarks

commit 2d074b5ddfbfbc486506384993bb5b269903e144

Add input pipeline and eval function to deepspeech model.
As part of this, I refactored tf_cnn_benchmark in the following way:

1. move all methods in data_utils.py to preprocessing.py, and make them member
   methods of InputPreprocessor. I need to do this as I need a custom
   create_dataset() method for librispeech dataset. In theory we can do it
   without this move but that requires plumbing parameters through the whole
   tf_cnn_benchmark stack which doesn't seem to be a good idea.
2. remove the get_data_type() method in benchmark_cnn.py, data_type become part
   of the model and is passed in the constructor of Model.
3. remove the code to set batch_size differently in distributed_all_reduce mode
   (line 2148 of benchmark_cnn.py), since it's wrong and will break
   InputPreprocessor.minibatch_fn() in distributed_all_reduce mode unless we
   add additional batch_size related parameters and plumb it though the whole
   stack.
4. make InputPreprocessor able to output more tensors than just input and
   label (DeepSpeech2 needs 4 input tensors)
5. refactor Model's API and reshaping logic in add_forward_pass_and_gradients()
   accordingly

PiperOrigin-RevId: 215422276

scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -37,7 +37,6 @@ import tensorflow as tf
 
 import cnn_util
 import constants
-import data_utils
 import datasets
 import flags
 import variable_mgr
@@ -50,6 +49,7 @@ from tensorflow.contrib.compiler import xla
 from tensorflow.core.protobuf import rewriter_config_pb2
 from tensorflow.python import debug as tf_debug
 from tensorflow.python.client import timeline
+from tensorflow.python.data.experimental.ops import prefetching_ops
 from tensorflow.python.framework import graph_util
 from tensorflow.python.framework import graph_util_impl
 from tensorflow.python.framework import importer
@@ -635,16 +635,6 @@ class GlobalStepWatcher(threading.Thread):
 
 class CheckpointNotFoundException(Exception):
   pass
-
-
-def get_data_type(params):
-  """Returns BenchmarkCNN's data type as determined by use_fp16.
-
-  Args:
-    params: Params tuple, typically created by make_params or
-            make_params_from_flags.
-  """
-  return tf.float16 if params.use_fp16 else tf.float32
 
 
 def create_config_proto(params):
@@ -2209,25 +2199,19 @@ class BenchmarkCNN(object):
 
     # Use prefetching mechanism provided by dataset input pipeline.
     if self.datasets_use_prefetch:
-      if self.params.variable_update == 'distributed_all_reduce':
-        batch_size = self.batch_size // len(self.devices)
-      else:
-        batch_size = self.batch_size
-
       if self.params.use_multi_device_iterator:
-        multi_device_iterator = data_utils.build_multi_device_iterator(
-            batch_size, len(self.devices),
-            self.input_preprocessor.parse_and_preprocess, self.cpu_device,
-            self.params, self.raw_devices, self.dataset)
+        multi_device_iterator = (
+            self.input_preprocessor.build_multi_device_iterator(
+                self.batch_size, len(self.devices), self.cpu_device,
+                self.params, self.raw_devices, self.dataset))
         return input_processing_info._replace(
             multi_device_iterator_input=multi_device_iterator.get_next())
 
       function_buffering_resources = (
-          data_utils.build_prefetch_input_processing(
-              batch_size, self.model.get_input_shape(),
-              len(self.devices), self.input_preprocessor.parse_and_preprocess,
-              self.cpu_device, self.params, self.devices,
-              get_data_type(self.params), self.dataset))
+          self.input_preprocessor.build_prefetch_input_processing(
+              self.batch_size, self.model.get_input_shapes(),
+              len(self.devices), self.cpu_device, self.params, self.devices,
+              self.model.get_input_data_types(), self.dataset))
       return input_processing_info._replace(
           function_buffering_resources=function_buffering_resources)
 
@@ -2238,28 +2222,28 @@ class BenchmarkCNN(object):
         subset = 'validation'
       else:
         subset = 'train'
-      images_splits, labels_splits = self.input_preprocessor.minibatch(
+      input_list = self.input_preprocessor.minibatch(
           self.dataset,
           subset=subset,
+          # TODO(laigd): consider removing this option, it should always use
+          # datasets.
           use_datasets=self.params.use_datasets,
           datasets_repeat_cached_sample=(
               self.params.datasets_repeat_cached_sample),
           shift_ratio=shift_ratio)
-      images_shape = images_splits[0].get_shape()
-      labels_shape = labels_splits[0].get_shape()
 
       input_producer_op = []
       input_producer_stages = []
       for device_num in range(len(self.devices)):
         staging_area = data_flow_ops.StagingArea(
-            [images_splits[0].dtype, labels_splits[0].dtype],
-            shapes=[images_shape, labels_shape],
-            shared_name='image_producer_staging_area_%d' % device_num)
+            [parts[0].dtype for parts in input_list],
+            shapes=[parts[0].get_shape() for parts in input_list],
+            shared_name='input_producer_staging_area_%d' % device_num)
         input_producer_stages.append(staging_area)
         for group_index in xrange(self.batch_group_size):
           batch_index = group_index + device_num * self.batch_group_size
           put_op = staging_area.put(
-              [images_splits[batch_index], labels_splits[batch_index]])
+              [parts[batch_index] for parts in input_list])
           input_producer_op.append(put_op)
       assert input_producer_op
 
@@ -2604,7 +2588,6 @@ class BenchmarkCNN(object):
                                      gpu_grad_stage_ops):
     """Add ops for forward-pass and gradient computations."""
     nclass = self.dataset.num_classes
-    data_type = get_data_type(self.params)
     if self.datasets_use_prefetch:
       function_buffering_resource = None
       if input_processing_info.function_buffering_resources:
@@ -2623,43 +2606,38 @@ class BenchmarkCNN(object):
       if function_buffering_resource is not None and input_data is not None:
         raise ValueError('Both function_buffering_resource and input_data '
                          'cannot be specified. Only one should be.')
-      input_shape = [self.model.get_batch_size()] + self.model.get_input_shape()
-      if function_buffering_resource is not None:
-        with tf.device(self.raw_devices[rel_device_num]):
-          inputs, labels = data_utils.get_inputs_and_labels(
-              function_buffering_resource, data_type)
-          inputs = tf.reshape(inputs, shape=input_shape)
-      else:
-        with tf.device(self.raw_devices[rel_device_num]):
-          labels, inputs = input_data
-          inputs = tf.reshape(inputs, shape=input_shape)
+      with tf.device(self.raw_devices[rel_device_num]):
+        if function_buffering_resource is not None:
+          input_list = prefetching_ops.function_buffering_resource_get_next(
+              function_buffering_resource,
+              output_types=self.model.get_input_data_types())
+        else:
+          input_list = input_data
     else:
       if not self.dataset.use_synthetic_gpu_inputs():
         input_producer_stage = input_processing_info.input_producer_stages[
             rel_device_num]
         with tf.device(self.cpu_device):
-          host_inputs, host_labels = input_producer_stage.get()
-          inputs_shape = host_inputs.get_shape()
-          labels_shape = host_labels.get_shape()
+          host_input_list = input_producer_stage.get()
         with tf.device(self.raw_devices[rel_device_num]):
           gpu_compute_stage = data_flow_ops.StagingArea(
-              [host_inputs.dtype, host_labels.dtype],
-              shapes=[inputs_shape, labels_shape])
+              [inp.dtype for inp in host_input_list],
+              shapes=[inp.get_shape() for inp in host_input_list])
           # The CPU-to-GPU copy is triggered here.
-          gpu_compute_stage_op = gpu_compute_stage.put(
-              [host_inputs, host_labels])
-          inputs, labels = gpu_compute_stage.get()
-          inputs = tf.reshape(inputs, shape=inputs_shape)
+          gpu_compute_stage_op = gpu_compute_stage.put(host_input_list)
+          input_list = gpu_compute_stage.get()
           gpu_compute_stage_ops.append(gpu_compute_stage_op)
       else:
         with tf.device(self.raw_devices[rel_device_num]):
           # Minor hack to avoid H2D copy when using synthetic data
-          inputs, labels = self.model.get_synthetic_inputs_and_labels(
-              BenchmarkCNN.GPU_CACHED_INPUT_VARIABLE_NAME, data_type, nclass)
+          input_list = self.model.get_synthetic_inputs(
+              BenchmarkCNN.GPU_CACHED_INPUT_VARIABLE_NAME, nclass)
 
-    labels_shape = self.model.get_labels_shape()
-    if labels_shape:
-      labels = tf.reshape(labels, shape=labels_shape)
+    input_shapes = self.model.get_input_shapes()
+    input_list = [
+        tf.reshape(input_list[i], shape=input_shapes[i])
+        for i in range(len(input_list))
+    ]
 
     def forward_pass_and_gradients():
       """Builds forward pass and gradient computation network.
@@ -2685,20 +2663,20 @@ class BenchmarkCNN(object):
       """
 
       build_network_result = self.model.build_network(
-          inputs, phase_train, nclass, data_type)
+          input_list, phase_train, nclass)
       logits = build_network_result.logits
 
       if not phase_train:
         return [logits]
 
-      base_loss = self.model.loss_function(build_network_result, labels)
+      base_loss = self.model.loss_function(input_list, build_network_result)
       params = self.variable_mgr.trainable_variables_on_device(
           rel_device_num, abs_device_num)
       l2_loss = None
       total_loss = base_loss
       with tf.name_scope('l2_loss'):
         fp32_params = params
-        if data_type == tf.float16 and self.params.fp16_vars:
+        if self.model.data_type == tf.float16 and self.params.fp16_vars:
           # fp16 reductions are very slow on GPUs, so cast to fp32 before
           # calling tf.nn.l2_loss and tf.add_n.
           # TODO(b/36217816): Once the bug is fixed, investigate if we should do
@@ -2810,7 +2788,7 @@ class BenchmarkCNN(object):
 
       if logits is not None:
         results['logits'] = logits
-        accuracy_ops = self.model.accuracy_function(logits, labels, data_type)
+        accuracy_ops = self.model.accuracy_function(input_list, logits)
         for name, op in accuracy_ops.items():
           results['accuracy:' + name] = op
 
@@ -2835,8 +2813,6 @@ class BenchmarkCNN(object):
     Returns:
       The image preprocessor, or None if synthetic data should be used.
     """
-    input_data_type = get_data_type(self.params)
-
     shift_ratio = 0
     if self.job_name:
       # shift_ratio prevents multiple workers from processing the same batch
@@ -2848,10 +2824,11 @@ class BenchmarkCNN(object):
     assert processor_class
     return processor_class(
         self.batch_size * self.batch_group_size,
-        self.model.get_input_shape(),
+        self.model.get_input_shapes(),
         len(self.devices) * self.batch_group_size,
-        dtype=input_data_type,
+        dtype=self.model.data_type,
         train=(not self.params.eval),
+        # TODO(laigd): refactor away image model specific parameters.
         distortions=self.params.distortions,
         resize_method=self.resize_method,
         shift_ratio=shift_ratio,

scripts/tf_cnn_benchmarks/all_reduce_benchmark.py
@@ -61,10 +61,9 @@ for name in flags.param_specs.keys():
 def get_var_shapes(model):
   """Returns the list of variable shapes for a tf_cnn_benchmarks Model."""
   with tf.Graph().as_default():
-    # The batch size of 2 is arbitrary, as the variable shapes do not depend on
-    # the batch size.
-    images = tf.placeholder(tf.float32, [2] + model.get_input_shape())
-    model.build_network(images)
+    # The variable shapes do not depend on the batch size.
+    images = tf.placeholder(tf.float32, model.get_input_shapes()[0])
+    model.build_network([images])
     return [[int(d) for d in v.shape.dims] for v in tf.trainable_variables()]
 
 

scripts/tf_cnn_benchmarks/benchmark_cnn_test.py
@@ -444,8 +444,10 @@ class TfCnnBenchmarksTest(tf.test.TestCase):
     benchmark_cnn.log_fn = test_util.print_and_add_to_list(logs)
     bench = benchmark_cnn.BenchmarkCNN(params)
     bench.input_preprocessor = preprocessing.TestImagePreprocessor(
-        params.batch_size * params.num_gpus, [227, 227, 3], params.num_gpus,
-        benchmark_cnn.get_data_type(params))
+        params.batch_size * params.num_gpus,
+        [[params.batch_size, 227, 227, 3], [params.batch_size]],
+        params.num_gpus,
+        bench.model.data_type)
     bench.dataset._queue_runner_required = True
     bench.input_preprocessor.set_fake_data(images, labels)
     bench.input_preprocessor.expected_subset = ('validation'

scripts/tf_cnn_benchmarks/data_utils.py
@@ -1,197 +1,0 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""tf.data utility methods.
-
-Collection of utility methods that make CNN benchmark code use tf.data easier.
-"""
-import tensorflow as tf
-
-from tensorflow.contrib.data.python.ops import batching
-from tensorflow.contrib.data.python.ops import interleave_ops
-from tensorflow.contrib.data.python.ops import threadpool
-from tensorflow.python.data.experimental.ops import prefetching_ops
-from tensorflow.python.data.ops import multi_device_iterator_ops
-from tensorflow.python.framework import function
-from tensorflow.python.platform import gfile
-
-
-def build_prefetch_input_processing(batch_size, data_point_shape, num_splits,
-                                    preprocess_fn, cpu_device, params,
-                                    gpu_devices, data_type, dataset):
-  """"Returns FunctionBufferingResources that do image pre(processing)."""
-  with tf.device(cpu_device):
-    if params.eval:
-      subset = 'validation'
-    else:
-      subset = 'train'
-
-    function_buffering_resources = []
-    remote_fn, args = minibatch_fn(
-        batch_size=batch_size,
-        data_point_shape=data_point_shape,
-        num_splits=num_splits,
-        preprocess_fn=preprocess_fn,
-        dataset=dataset,
-        subset=subset,
-        train=(not params.eval),
-        datasets_repeat_cached_sample=params.datasets_repeat_cached_sample,
-        num_threads=params.datasets_num_private_threads,
-        datasets_use_caching=params.datasets_use_caching,
-        datasets_parallel_interleave_cycle_length=(
-            params.datasets_parallel_interleave_cycle_length),
-        datasets_sloppy_parallel_interleave=(
-            params.datasets_sloppy_parallel_interleave))
-    for device_num in range(len(gpu_devices)):
-      with tf.device(gpu_devices[device_num]):
-        buffer_resource_handle = prefetching_ops.function_buffering_resource(
-            f=remote_fn,
-            output_types=[data_type, tf.int32],
-            target_device=cpu_device,
-            string_arg=args[0],
-            buffer_size=params.datasets_prefetch_buffer_size,
-            shared_name=None)
-        function_buffering_resources.append(buffer_resource_handle)
-    return function_buffering_resources
-
-
-def build_multi_device_iterator(batch_size, num_splits, preprocess_fn,
-                                cpu_device, params, gpu_devices, dataset):
-  """Creates a MultiDeviceIterator."""
-  assert num_splits == len(gpu_devices)
-  with tf.name_scope('batch_processing'):
-    if params.eval:
-      subset = 'validation'
-    else:
-      subset = 'train'
-    batch_size_per_split = batch_size // num_splits
-    ds = create_dataset(
-        batch_size,
-        num_splits,
-        batch_size_per_split,
-        preprocess_fn,
-        dataset,
-        subset,
-        train=(not params.eval),
-        datasets_repeat_cached_sample=params.datasets_repeat_cached_sample,
-        num_threads=params.datasets_num_private_threads,
-        datasets_use_caching=params.datasets_use_caching,
-        datasets_parallel_interleave_cycle_length=(
-            params.datasets_parallel_interleave_cycle_length),
-        datasets_sloppy_parallel_interleave=(
-            params.datasets_sloppy_parallel_interleave))
-    multi_device_iterator = multi_device_iterator_ops.MultiDeviceIterator(
-        ds,
-        gpu_devices,
-        source_device=cpu_device,
-        max_buffer_size=params.multi_device_iterator_max_buffer_size)
-    tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS,
-                         multi_device_iterator.initializer)
-    return multi_device_iterator
-
-
-def get_inputs_and_labels(function_buffering_resource, data_type):
-  """Given a FunctionBufferingResource obtains images and labels from it."""
-  return prefetching_ops.function_buffering_resource_get_next(
-      function_buffer_resource=function_buffering_resource,
-      output_types=[data_type, tf.int32])
-
-
-def create_dataset(batch_size,
-                   num_splits,
-                   batch_size_per_split,
-                   preprocess_fn,
-                   dataset,
-                   subset,
-                   train,
-                   datasets_repeat_cached_sample,
-                   num_threads=None,
-                   datasets_use_caching=False,
-                   datasets_parallel_interleave_cycle_length=None,
-                   datasets_sloppy_parallel_interleave=False):
-  """Creates a dataset for the benchmark."""
-  glob_pattern = dataset.tf_record_pattern(subset)
-  file_names = gfile.Glob(glob_pattern)
-  if not file_names:
-    raise ValueError('Found no files in --data_dir matching: {}'
-                     .format(glob_pattern))
-  ds = tf.data.TFRecordDataset.list_files(file_names)
-  ds = ds.apply(
-      interleave_ops.parallel_interleave(
-          tf.data.TFRecordDataset,
-          cycle_length=datasets_parallel_interleave_cycle_length or 10,
-          sloppy=datasets_sloppy_parallel_interleave))
-  if datasets_repeat_cached_sample:
-    # Repeat a single sample element indefinitely to emulate memory-speed IO.
-    ds = ds.take(1).cache().repeat()
-  counter = tf.data.Dataset.range(batch_size)
-  counter = counter.repeat()
-  ds = tf.data.Dataset.zip((ds, counter))
-  ds = ds.prefetch(buffer_size=batch_size)
-  if datasets_use_caching:
-    ds = ds.cache()
-  if train:
-    ds = ds.apply(tf.contrib.data.shuffle_and_repeat(buffer_size=10000))
-  else:
-    ds = ds.repeat()
-  ds = ds.apply(
-      batching.map_and_batch(
-          map_func=preprocess_fn,
-          batch_size=batch_size_per_split,
-          num_parallel_batches=num_splits))
-  ds = ds.prefetch(buffer_size=num_splits)
-  if num_threads:
-    ds = threadpool.override_threadpool(
-        ds,
-        threadpool.PrivateThreadPool(
-            num_threads, display_name='input_pipeline_thread_pool'))
-  return ds
-
-
-def create_iterator(ds):
-  ds_iterator = ds.make_initializable_iterator()
-  tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS, ds_iterator.initializer)
-  return ds_iterator
-
-
-def minibatch_fn(batch_size, data_point_shape, num_splits, preprocess_fn,
-                 dataset, subset, train, datasets_repeat_cached_sample,
-                 num_threads, datasets_use_caching,
-                 datasets_parallel_interleave_cycle_length,
-                 datasets_sloppy_parallel_interleave):
-  """Returns a function and list of args for the fn to create a minibatch."""
-  batch_size_per_split = batch_size // num_splits
-  with tf.name_scope('batch_processing'):
-    ds = create_dataset(batch_size, num_splits, batch_size_per_split,
-                        preprocess_fn, dataset, subset, train,
-                        datasets_repeat_cached_sample, num_threads,
-                        datasets_use_caching,
-                        datasets_parallel_interleave_cycle_length,
-                        datasets_sloppy_parallel_interleave)
-
-    ds_iterator = create_iterator(ds)
-
-    ds_iterator_string_handle = ds_iterator.string_handle()
-
-    @function.Defun(tf.string)
-    def _fn(h):
-      remote_iterator = tf.data.Iterator.from_string_handle(
-          h, ds_iterator.output_types, ds_iterator.output_shapes)
-      labels, inputs = remote_iterator.get_next()
-      inputs = tf.reshape(
-          inputs, shape=[batch_size_per_split] + data_point_shape)
-      labels = tf.reshape(labels, [batch_size_per_split])
-      return inputs, labels
-
-    return _fn, [ds_iterator_string_handle]

scripts/tf_cnn_benchmarks/datasets.py
@@ -84,10 +84,19 @@ class Dataset(object):
 
 
 class LibrispeechDataset(Dataset):
+  """Configuration for LibriSpeech dataset."""
 
   def __init__(self, data_dir=None):
     super(LibrispeechDataset, self).__init__(
         'librispeech', data_dir, num_classes=29)
+
+  def tf_record_pattern(self, subset):
+    if subset == 'train':
+      return os.path.join(self.data_dir, 'train-clean-*.tfrecords')
+    elif subset == 'validation':
+      return os.path.join(self.data_dir, 'test-clean.tfrecords')
+    else:
+      return ''
 
   def num_examples_per_epoch(self, subset='train'):
     del subset
@@ -212,8 +221,7 @@ _SUPPORTED_INPUT_PREPROCESSORS = {
         'default': preprocessing.Cifar10ImagePreprocessor
     },
     'librispeech': {
-        # TODO(laigd): add preprocessor for librispeech dataset.
-        # 'default': preprocessing.LibrispeechPreprocessor
+        'default': preprocessing.LibrispeechPreprocessor
     },
     'coco': {
         'default': preprocessing.COCOPreprocessor

commit d954648fd21cae094e55739a854039126f4932f5

Fix performance regression.
The regression was caused by 2d074b5ddfbfbc486506384993bb5b269903e144.

Performance numbers for the following command on a Volta GCE instance. 5 iterations of each row was run and the average was taken. For real data, --data_dir was added:

python tf_cnn_benchmarks.py --data_format=NCHW --batch_size=256 --num_batches=100 --model=resnet50_v1.5 --optimizer=momentum --variable_update=replicated --all_reduce_spec=nccl --use_fp16=True --nodistortions --gradient_repacking=2 --datasets_use_prefetch=True --per_gpu_thread_count=1 --local_parameter_device=gpu --num_gpus=8 --display_every=10

Synthetic data with this change:    5888.322
Synthetic data without this change: 2744.608
Synthetic data before regression:   5872.164

Real data with this change:    5603.346
Real data without this change: 3771.412
Real data before regression:   5523.506

Interestingly, now real data is 5603.346/5523.506 = 1.45% faster than before. I am not sure why.

PiperOrigin-RevId: 215754238

scripts/tf_cnn_benchmarks/benchmark_cnn.py
@@ -2633,11 +2633,12 @@ class BenchmarkCNN(object):
           input_list = self.model.get_synthetic_inputs(
               BenchmarkCNN.GPU_CACHED_INPUT_VARIABLE_NAME, nclass)
 
-    input_shapes = self.model.get_input_shapes()
-    input_list = [
-        tf.reshape(input_list[i], shape=input_shapes[i])
-        for i in range(len(input_list))
-    ]
+    with tf.device(self.devices[rel_device_num]):
+      input_shapes = self.model.get_input_shapes()
+      input_list = [
+          tf.reshape(input_list[i], shape=input_shapes[i])
+          for i in range(len(input_list))
+      ]
 
     def forward_pass_and_gradients():
       """Builds forward pass and gradient computation network.