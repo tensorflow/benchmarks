db0ab7a514c6e995ddee0c3124e56b0dd15726fb
jayhpark530 <jayhpark530@gmail.com>
2019-01-31 10:37:25 +0900

    change the number of kerners to the settings of the alexnet paper.

--- scripts/tf_cnn_benchmarks/models/alexnet_model.py
+++ scripts/tf_cnn_benchmarks/models/alexnet_model.py
@@ -37,9 +37,9 @@ class AlexnetModel(model.CNNModel):
 
   def add_inference(self, cnn):
     # Note: VALID requires padding the images by 3 in width and height
-    cnn.conv(64, 11, 11, 4, 4, 'VALID')
+    cnn.conv(96, 11, 11, 4, 4, 'VALID')
     cnn.mpool(3, 3, 2, 2)
-    cnn.conv(192, 5, 5)
+    cnn.conv(256, 5, 5)
     cnn.mpool(3, 3, 2, 2)
     cnn.conv(384, 3, 3)
     cnn.conv(384, 3, 3)
