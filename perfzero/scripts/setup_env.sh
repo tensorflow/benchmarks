747e8da759987ddb4b81e50c1b60b50e809eaa84
Dong Lin <donglin@google.com>
2019-02-04 10:56:30 -0800

    Add fake_tf_record_data to setup_env.sh

--- perfzero/scripts/setup_env.sh
+++ perfzero/scripts/setup_env.sh
@@ -10,6 +10,6 @@ export PERFZERO_TEST_ENV=local
 export PERFZERO_PYTHON_PATH=models,benchmarks/scripts/tf_cnn_benchmarks
 export PERFZERO_GIT_REPOS="models;https://github.com/tensorflow/models.git,benchmarks;https://github.com/tensorflow/benchmarks.git"
 export PERFZERO_OUTPUT_GCS_URL=gs://tf-performance/test-results
-export PERFZERO_GCS_DOWNLOADS="cifar10_data;gs://tf-perf-imagenet-uswest1/tensorflow/cifar10_data/*"
+export PERFZERO_GCS_DOWNLOADS="cifar10_data;gs://tf-perf-imagenet-uswest1/tensorflow/cifar10_data/*,gs://tf-perf-imagenet-uswest1/tensorflow/fake_tf_record_data"
 export PERFZERO_BIGQUERY_TABLE_NAME="benchmark_results_dev.result"
 export PERFZERO_BIGQUERY_PROJECT_NAME="google.com:tensorflow-performance"
