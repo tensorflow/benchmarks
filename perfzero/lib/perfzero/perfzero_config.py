a926a4d8aa7bf1656229220771454ac7d7b0dcf9
Toby Boyd <tobyboyd@google.com>
2019-02-07 10:16:56 -0800

    Removed roguezero envars and all old upload logic
    - also fixed ~95% of lint warnings

--- perfzero/lib/perfzero/perfzero_config.py
+++ perfzero/lib/perfzero/perfzero_config.py
@@ -23,7 +23,7 @@ class PerfZeroConfig(object):
 
   def __init__(self, mode):
     # In this mode, PERFZERO_BENCHMARK_CLASS and PERFZERO_BENCHMARK_METHODS
-    # are used to dertermine the benchmark methods we want to execute. This mode is
+    # are used to determine the benchmark methods to execute. This mode is
     # deprecated because it constraints one perfzero execution to only run
     # benchmark methods from one class
     if mode == 'env' and 'PERFZERO_BENCHMARK_CLASS' in os.environ:
@@ -32,10 +32,10 @@ class PerfZeroConfig(object):
       benchmark_class_str = self._get_env_var('PERFZERO_BENCHMARK_CLASS')
       self.benchmark_methods_maybe_filter = []
       for benchmark_method in benchmark_methods_str.split(','):
-        self.benchmark_methods_maybe_filter.append(benchmark_class_str + '.' + benchmark_method)
+        self.benchmark_methods_maybe_filter.append(benchmark_class_str + '.' +
+                                                   benchmark_method)
       self.platform_name_str = self._get_env_var('PERFZERO_PLATFORM_NAME')
       self.system_name_str = self._get_env_var('PERFZERO_SYSTEM_NAME')
-      self.test_env_str = self._get_env_var('PERFZERO_TEST_ENV')
       self.python_path_str = self._get_env_var('PERFZERO_PYTHON_PATH')
       self.git_repos_str = self._get_env_var('PERFZERO_GIT_REPOS')
       self.output_gcs_url_str = self._get_env_var('PERFZERO_OUTPUT_GCS_URL',
@@ -50,7 +50,8 @@ class PerfZeroConfig(object):
                                                    False, 'docker/Dockerfile')
       self.ml_framework_build_label_str = self._get_env_var(
           'PERFZERO_ML_FRAMEWORK_BUILD_LABEL', False)
-      self.execution_label_str = self._get_env_var('PERFZERO_EXECUTION_LABEL', False)
+      self.execution_label_str = self._get_env_var('PERFZERO_EXECUTION_LABEL',
+                                                   False)
     # In this mode, environment variables with prefix PERFZERO_BENCHMARK_METHOD
     # are used to dertermine the benchmark methods we want to execute. The
     # value of these environment variables encodes both the benchmark class and
@@ -78,43 +79,15 @@ class PerfZeroConfig(object):
                                                    False, 'docker/Dockerfile')
       self.ml_framework_build_label_str = self._get_env_var(
           'PERFZERO_ML_FRAMEWORK_BUILD_LABEL', False)
-      self.execution_label_str = self._get_env_var('PERFZERO_EXECUTION_LABEL', False)
-    # In this mode, ROGUE_TEST_CLASS and ROGUE_TEST_METHODS
-    # are used to dertermine the benchmark methods we want to execute. This mode is
-    # deprecated because the variable name should start with PERFZERO
-    elif mode == 'env' and 'ROGUE_TEST_CLASS' in os.environ:
-      benchmark_methods_str = self._get_env_var('ROGUE_TEST_METHODS')
-      benchmark_class_str = self._get_env_var('ROGUE_TEST_CLASS')
-      self.benchmark_methods_maybe_filter = []
-      for benchmark_method in benchmark_methods_str.split(','):
-        self.benchmark_methods_maybe_filter.append(benchmark_class_str + '.' + benchmark_method)
-      self.platform_name_str = self._get_env_var(
-          'ROGUE_PLATFORM', False, default='unknown')
-      self.system_name_str = self._get_env_var(
-          'ROGUE_PLATFORM_TYPE', False, default='unknown')
-      self.test_env_str = self._get_env_var(
-          'ROGUE_TEST_ENV', False, default='local')
-      self.python_path_str = self._get_env_var('ROGUE_PYTHON_PATH')
-      self.git_repos_str = self._get_env_var('ROGUE_GIT_REPOS')
-      self.output_gcs_url_str = self._get_env_var(
-          'ROGUE_CODE_DIR', False, default='')
-      self.gcs_downloads_str = self._get_env_var(
-          'ROGUE_GCS_DOWNLOADS', False, default='')
-      self.bigquery_project_name_str = self._get_env_var(
-          'ROGUE_REPORT_PROJECT', False, default='')
-      self.bigquery_dataset_table_name_str = 'benchmark_results.result'
-      self.ml_framework_build_label_str = None
-      self.execution_label_str = None
-      if not self.bigquery_project_name_str:
-        self.bigquery_project_name_str = 'google.com:tensorflow-performance'
-        self.bigquery_dataset_table_name_str = 'benchmark_results_dev.result'
+      self.execution_label_str = self._get_env_var('PERFZERO_EXECUTION_LABEL',
+                                                   False)
     elif mode == 'mock':
       pass
 
   def get_env_vars(self):
     env_vars = {}
     for key in os.environ.keys():
-      if key.startswith('PERFZERO_') or key.startswith('ROGUE_'):
+      if key.startswith('PERFZERO_'):
         env_vars[key] = os.environ[key]
     return env_vars
 
--- perfzero/scripts/setup_env.sh
+++ perfzero/scripts/setup_env.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-#export PERFZERO_BENCHMARK_METHOD=leading_indicators_test.Resnet50Benchmarks.benchmark_fake_1gpu_gpuparams
-#export PERFZERO_BENCHMARK_METHODS=benchmark_fake_1gpu_gpuparams
-#export PERFZERO_BENCHMARK_CLASS=leading_indicators_test.Resnet50Benchmarks
 export PERFZERO_BENCHMARK_METHODS=unit_test
 export PERFZERO_BENCHMARK_CLASS=official.resnet.estimator_cifar_benchmark.EstimatorCifar10BenchmarkTests
 export PERFZERO_PLATFORM_NAME=workstation
 export PERFZERO_SYSTEM_NAME=z420
-export PERFZERO_TEST_ENV=local
 export PERFZERO_PYTHON_PATH=models,benchmarks/scripts/tf_cnn_benchmarks
 export PERFZERO_GIT_REPOS="https://github.com/tensorflow/models.git,https://github.com/tensorflow/benchmarks.git"
 export PERFZERO_OUTPUT_GCS_URL=gs://tf-performance/test-results
 export PERFZERO_GCS_DOWNLOADS="gs://tf-perf-imagenet-uswest1/tensorflow/cifar10_data/"
-export PERFZERO_BIGQUERY_TABLE_NAME="benchmark_results_dev.result"
+export PERFZERO_BIGQUERY_TABLE_NAME="perfzero_dev.benchmark_summary"
 export PERFZERO_BIGQUERY_PROJECT_NAME="google.com:tensorflow-performance"
+export PERFZERO_EXECUTION_LABEL=perfzero_development
+export PERFZERO_ML_FRAMEWORK_BUILD_LABEL=nightly-gpu
