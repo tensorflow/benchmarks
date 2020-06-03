04657048b2dac4f9daf3fd4b27d218d4d808c7d6
sganeshb <53017143+sganeshb@users.noreply.github.com>
2020-06-03 16:10:54 -0700

    Update tpu_utils.py

--- perfzero/lib/perfzero/tpu_utils.py
+++ perfzero/lib/perfzero/tpu_utils.py
@@ -1,10 +1,8 @@
 """Utility to manage the tpu version before starting the benchmark."""
 
 import json
+import logging
 
-from absl import app
-from absl import flags
-from absl import logging
 from six.moves.urllib import request
 
 try:
@@ -14,12 +12,6 @@ except ImportError:
       'Falling back to TensorFlow client; we recommended you install the Cloud '
       'TPU client directly with pip install cloud-tpu-client.')
   from tensorflow.python.tpu.client import client  # pylint: disable=g-import-not-at-top
-
-flags.DEFINE_string('started_tpu_address', None,
-                    'The TPU address after the tpu was started.')
-flags.DEFINE_string('update_tpu_vm_version', None,
-                    'The tpu vm version to update the current tpu.')
-FLAGS = flags.FLAGS
 
 
 def _as_text(s):
@@ -54,25 +46,3 @@ def get_tpu_version(tpu_address):
   else:
     logging.error('No tpu endpoint info')
     return None
-
-
-def main(unused_argv):
-  tpu_address = FLAGS.started_tpu_address
-  new_vm_id = FLAGS.update_tpu_vm_version
-  logging.info('tpu_address: %s, update request: %s', tpu_address, new_vm_id)
-
-  tpu_client = client.Client(tpu=tpu_address)
-  logging.info('Waiting for initial tpu healthy.')
-  tpu_client.wait_for_healthy()
-  logging.info('Initial tpu healthy.')
-
-  if new_vm_id:
-    logging.info('Trying configure_tpu_version(%s).', new_vm_id)
-    tpu_client.configure_tpu_version(version=new_vm_id)
-    logging.info('Waiting for new healthy.')
-    tpu_client.wait_for_healthy()
-    logging.info('Health check done.')
-
-
-if __name__ == '__main__':
-  app.run(main)
