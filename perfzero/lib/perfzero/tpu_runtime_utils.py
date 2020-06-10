25ffeea7a6b188bb9b20d3816e11a4cb4144ca34
sganeshb <53017143+sganeshb@users.noreply.github.com>
2020-06-10 13:00:16 -0700

    Update tpu_runtime_utils.py

--- perfzero/lib/perfzero/tpu_runtime_utils.py
+++ perfzero/lib/perfzero/tpu_runtime_utils.py
@@ -13,11 +13,13 @@ except ImportError:
       'TPU client directly with pip install cloud-tpu-client.')
   from tensorflow.python.tpu.client import client  # pylint: disable=g-import-not-at-top
 
+
 def _as_text(s):
   """Converts a byte/string into string."""
   if isinstance(s, bytes):
     return s.decode('utf-8')
   return s
+
 
 def _get_content(url):
   """Opens the url and loads the response into json."""
@@ -28,17 +30,19 @@ def _get_content(url):
   logging.info('response text = %s', resp_text)
   return json.loads(resp_text)
 
+
 def _get_version_info(url):
   """Constructs a version info from the response."""
   json_data = _get_content(url)
   logging.info('json_data = %s', json_data)
   info = {
-    'url': '',
-    'hash': json_data.get('buildLabel', ''),
-    'branch': '',
-    'piper_id': json_data.get('piperOriginRevId', '')
+      'url': '',
+      'hash': json_data.get('buildLabel', ''),
+      'branch': '',
+      'piper_id': json_data.get('piperOriginRevId', '')
   }
   return info
+
 
 def get_tpu_version(tpu_address):
   """Returns the current software version on tpu."""
@@ -53,11 +57,12 @@ def get_tpu_version(tpu_address):
   else:
     logging.error('No tpu endpoint info')
     return {
-      'url': '',
-      'hash': '',
-      'branch': '',
-      'piper_id': '',
+        'url': '',
+        'hash': '',
+        'branch': '',
+        'piper_id': '',
     }
-  
+
+
 def configure_tpu(tpu_params):
   return get_tpu_version(tpu_params.get('name'))
