0f8022724ed956c1581e128deee2b3239a121db8
sganeshb <53017143+sganeshb@users.noreply.github.com>
2020-06-10 12:58:06 -0700

    Update tpu_runtime_utils.py

--- perfzero/lib/perfzero/tpu_runtime_utils.py
+++ perfzero/lib/perfzero/tpu_runtime_utils.py
@@ -13,13 +13,11 @@ except ImportError:
       'TPU client directly with pip install cloud-tpu-client.')
   from tensorflow.python.tpu.client import client  # pylint: disable=g-import-not-at-top
 
-
 def _as_text(s):
   """Converts a byte/string into string."""
   if isinstance(s, bytes):
     return s.decode('utf-8')
   return s
-
 
 def _get_content(url):
   """Opens the url and loads the response into json."""
@@ -28,33 +26,38 @@ def _get_content(url):
   resp = request.urlopen(req)
   resp_text = _as_text(resp.read())
   logging.info('response text = %s', resp_text)
-  json_data = json.loads(resp_text)
+  return json.loads(resp_text)
+
+def _get_version_info(url):
+  """Constructs a version info from the response."""
+  json_data = _get_content(url)
   logging.info('json_data = %s', json_data)
-  return json_data
+  info = {
+    'url': '',
+    'hash': json_data.get('buildLabel', ''),
+    'branch': '',
+    'piper_id': json_data.get('piperOriginRevId', '')
+  }
+  return info
 
 def get_tpu_version(tpu_address):
   """Returns the current software version on tpu."""
   logging.info('Trying to connect to tpu %s', tpu_address)
   tpu_client = client.Client(tpu=tpu_address)
   tpu_client.wait_for_healthy()
-  runtime_version = tpu_client.runtime_version()
-  logging.info('runtime_version returned: %s', runtime_version)
-  local_ip = tpu_client.get_local_ip()
-  logging.info('local_ip: %s', local_ip)
-  
-  #url = 'http://{}:8475/requestversion'.format(local_ip)
-  #return _get_content(url)
-
-  # The below line fails with 501 - not implemented
   workers = tpu_client.network_endpoints()
   if workers:
     ip_addr = workers[0]['ipAddress']
     url = 'http://{}:8475/requestversion'.format(ip_addr)
-    return _get_content(url)
+    return _get_version_info(url)
   else:
     logging.error('No tpu endpoint info')
-    return None
-  
+    return {
+      'url': '',
+      'hash': '',
+      'branch': '',
+      'piper_id': '',
+    }
   
 def configure_tpu(tpu_params):
-  get_tpu_version(tpu_params.get('name'))
+  return get_tpu_version(tpu_params.get('name'))
