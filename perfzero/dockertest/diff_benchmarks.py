ee85606249d9353bd31484c573501fa165ea2eec
sganeshb <53017143+sganeshb@users.noreply.github.com>
2021-02-18 11:07:45 -0800

    Update diff_benchmarks.py

--- perfzero/dockertest/diff_benchmarks.py
+++ perfzero/dockertest/diff_benchmarks.py
@@ -1,103 +1,117 @@
-#!/bin/bash
-# Copyright 2021 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# Lint as: python3
+# # Copyright 2021 The TensorFlow Authors. All Rights Reserved.
+# #
+# # Licensed under the Apache License, Version 2.0 (the "License");
+# # you may not use this file except in compliance with the License.
+# # You may obtain a copy of the License at
+# #
+# #     http://www.apache.org/licenses/LICENSE-2.0
+# #
+# # Unless required by applicable law or agreed to in writing, software
+# # distributed under the License is distributed on an "AS IS" BASIS,
+# # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# # See the License for the specific language governing permissions and
+# # limitations under the License.
+# #
 # ==============================================================================
+"""Simple script to diff benchmark results.
 
-set -e
-set -x
+This script will read all the summary files from a base output directory and
+print a human readable diff report.
+"""
 
-git clone https://github.com/tensorflow/benchmarks.git
-cd benchmarks
+import json
+import os
+import sys
 
-tf_spec="tf-nightly-gpu==2.5.0.dev20210212"
 
-CIFAR10_DATA="gs://tf-perf-imagenet-uswest1/tensorflow/cifar10_data/cifar-10-batches-bin"
-CIFAR10_BENCHMARKS="official.benchmark.keras_cifar_benchmark.Resnet56KerasBenchmarkReal.benchmark_1_gpu_no_dist_strat"
+def _find_perfzero_logs(docker_output_dir):
+  """Finds pairs of json_file, output_log file from all methods."""
+  summary_files = []
+  for root, _, files in os.walk(docker_output_dir):
+    for summary_file in files:
+      if summary_file.endswith('perfzero_summary.json'):
+        full_summary_file = os.path.join(root, summary_file)
+        summary_files.append(full_summary_file)
+        sys.stdout.write('Found json {}\n'.format(full_summary_file))
+  return summary_files
 
-RESNET50_DATA="gs://tf-perf-imagenet-uswest1/tensorflow/imagenet"
-RESNET50_BENCHMARKS="official.benchmark.resnet_ctl_imagenet_benchmark.Resnet50CtlBenchmarkReal.benchmark_1_gpu_fp16"
 
-function run_single_benchmark() {
-  docker_name=$1
-  label=$2
-  data_downloads=$3
-  benchmark_methods=$4
+def _load_summaries(summary_files):
+  """Loads input json file paths and returns json objects."""
+  summary_jsons = []
+  for summary_file in summary_files:
+    with open(summary_file, 'r') as f:
+      summary_json = json.load(f)
+      summary_jsons.append(summary_json)
+  return summary_jsons
 
-  perfzero_pwd=`pwd`
 
-  nvidia-docker run \
-    -v ${perfzero_pwd}:/workspace \
-    -v /data:/data \
-    -e PERFZERO_RUN_TAGS= \
-    -e PERFZERO_TRACKING_ID= \
-    -e PERFZERO_COMMIT_LABEL= \
-    -e PERFZERO_EXECUTION_BRANCH=master \
-    ${docker_name} \
-    python3 /workspace/perfzero/lib/benchmark.py \
-    --bigquery_dataset_table_name="" \
-    --data_downloads="${data_downloads}" \
-    --ml_framework_build_label=v2-nightly-gpu \
-    --execution_label="${label}" \
-    --platform_name=kokoro-gcp \
-    --system_name=n1-standard-8-1xA100 \
-    --output_gcs_url="" \
-    --benchmark_class_type= \
-    --scratch_gcs_url= \
-    --root_data_dir=/data \
-    --benchmark_num_trials=2 \
-    --bigquery_project_name="" \
-    --git_repos="https://github.com/tensorflow/models.git;benchmark" \
-    --python_path=models \
-    --benchmark_methods=${benchmark_methods} \
-    --result_upload_methods="" \
-    --gcloud_key_file_url="${PERFZERO_GCLOUD_KEY_FILE_URL}" \
-    --tpu_parameters=
-}
+def _summarize_benchmarks(summary_files):
+  """Remaps list of json files -> summaries by benchmark method."""
+  summary_jsons = _load_summaries(summary_files)
+  performance_by_method = {}
 
-function run_benchmarks() {
-  docker_name=$1
-  label=$2
+  for summary_json in summary_jsons:
+    method = summary_json['benchmark_result']['name']
+    trial = summary_json['benchmark_result']['trial_id']
+    metrics_list = summary_json['benchmark_result']['metrics']
+    metrics = {}
+    for metric_info in metrics_list:
+      metrics[metric_info['name']] = metric_info['value']
+    metrics['wall_time'] = summary_json['benchmark_result']['wall_time']
+    label = summary_json['benchmark_info']['execution_label']
 
-  run_single_benchmark ${docker_name} ${label} "${CIFAR10_DATA}" "${CIFAR10_BENCHMARKS}"
-  run_single_benchmark ${docker_name} ${label} "${RESNET50_DATA}" "${RESNET50_BENCHMARKS}"
-}
+    performance_by_method.setdefault(method, {}).setdefault(label, [])
+    performance_by_method[method][label].append((trial, metrics))
 
-function setup_docker() {
-  label=$1
-  dockerfile=$2
+  return performance_by_method
 
-  echo "`date` Setting up ${label} docker..."
-  sudo python3 perfzero/lib/setup.py \
-    --gce_nvme_raid= \
-    --docker_tag="${label}" \
-    --gcloud_key_file_url= \
-    --tensorflow_pip_spec=${tf_spec} \
-    --dockerfile_path=${dockerfile}
-  echo "`date` Finished setting up ${label} docker."
-}
 
-function diff_benchmarks() {
-  python3 perfzero/dockertest/diff_benchmarks.py `pwd`
-}
+def _print_diff_report(performance_by_method):
+  """Prints a diff report of benchmark performance."""
+  print('Performance report:')
+  print(json.dumps(performance_by_method, indent=2))
 
-baseline_docker="docker/Dockerfile_ubuntu_1804_tf_cuda_11"
-experiment_docker="docker/Dockerfile_ubuntu_cuda11_8_0_0_180"
+  method_to_metric_to_perf = {}
+  for method in performance_by_method:
+    for label, label_data in performance_by_method[method].items():
+      latest_trial_data = max(label_data, key=lambda x: x[0])
+      latest_metrics = latest_trial_data[1]
+      for metric, value in latest_metrics.items():
+        method_to_metric_to_perf.setdefault(method, {}).setdefault(metric, [])
+        method_to_metric_to_perf[method][metric].append((label, value))
 
-setup_docker "control/tensorflow" ${baseline_docker}
-run_benchmarks "control/tensorflow" "control-8-0-4-30"
+  print('Diff report:')
+  for method in sorted(method_to_metric_to_perf):
+    print('-- benchmark: {}'.format(method))
+    for metric in sorted(method_to_metric_to_perf[method].keys()):
+      value_list = []
+      for label, value in sorted(
+          method_to_metric_to_perf[method][metric], key=lambda x: x[0]):
+        print('    ---- {}: {}'.format(label, value))
+        value_list.append(value)
 
-setup_docker "experiment/tensorflow" ${experiment_docker}
-run_benchmarks "experiment/tensorflow" "experiment-8-0-0-180"
+      if len(value_list) == 2:
+        control_val = float(value_list[0])
+        expt_val = float(value_list[1])
+        if abs(control_val) > 1e-5:
+          diff_pct = (expt_val / control_val - 1.0) * 100.0
+        else:
+          diff_pct = -1.0
+        print('    ---- diff: {:2.3f}%'.format(diff_pct))
 
-diff_benchmarks
+
+def main():
+  if len(sys.argv) != 2:
+    raise RuntimeError('Usage: {} <base perfzero output dir>'.format(
+        sys.argv[0]))
+
+  perfzero_output_dir = sys.argv[1]
+  summary_files = _find_perfzero_logs(perfzero_output_dir)
+  performance_by_method = _summarize_benchmarks(summary_files)
+  _print_diff_report(performance_by_method)
+
+
+if __name__ == '__main__':
+  main()
